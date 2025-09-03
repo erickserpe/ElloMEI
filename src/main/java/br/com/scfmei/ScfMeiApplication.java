3a211b3b7fffd0c1349b34ab2393f58a570d82e4
Erick Luan Serpe Taborda <serpetabordaerickluan@gmail.com>
2025-09-03 17:06:46 -0300

    Initial commit

---
+++ src/main/java/br/com/scfmei/ScfMeiApplication.java
@@ -0,0 +1,13 @@
+package br.com.scfmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfMeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScfMeiApplication.class, args);
+    }
+
+}
